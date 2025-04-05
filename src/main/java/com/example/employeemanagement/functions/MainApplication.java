4cfa83bb894d54ad905ba2ae1d3f36178c0e116e
Edison Zheng <sketchyazx@gmail.com>
2025-04-04 20:58:49 -0400

    Create MainApplication.java

---
+++ src/main/java/com/example/employeemanagement/functions/MainApplication.java
@@ -0,0 +1,11 @@
+package com.example.employeelogin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeLoginApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(EmployeeLoginApplication.class, args);
+    }
+}
