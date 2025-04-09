6e2c23ee446fcc9a9a8928ca101b10b43f31ee2c
Avihhan <109458161+avihhan@users.noreply.github.com>
2025-04-09 17:55:26 +0000

    cors issues tackle

--- src/main/java/com/example/employeemanagement/Application.java
+++ src/main/java/com/example/employeemanagement/Application.java
@@ -2,9 +2,13 @@ package com.example.employeemanagement;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class Application {
+
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
@@ -15,8 +19,10 @@ public class Application {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("*")
-                        .allowedMethods("*");
+                    .allowedOrigins("*")
+                    .allowedMethods("*")
+                    .allowedHeaders("*")
+                    .allowCredentials(true);
             }
         };
     }
