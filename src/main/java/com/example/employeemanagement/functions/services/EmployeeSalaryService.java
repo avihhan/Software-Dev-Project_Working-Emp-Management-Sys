461112455967516f195925db3828b6d9474edea2
Alex Kray <42243720+alkray@users.noreply.github.com>
2025-05-04 21:23:22 +0000

    Updating db connection

--- src/main/java/com/example/employeemanagement/functions/services/EmployeeSalaryService.java
+++ src/main/java/com/example/employeemanagement/functions/services/EmployeeSalaryService.java
@@ -10,9 +10,9 @@ import org.springframework.stereotype.Service;
 @Service
 public class EmployeeSalaryService {
     // Database connection
-    private static final String DB_URL = "jdbc:mysql://34.29.27.190:3306/employeedata";
-    private static final String DB_USER = "root";
-    private static final String DB_PASSWORD = "root";
+    private static final String DB_URL = "jdbc:mysql://database-1.c1qc88ayc19c.us-east-2.rds.amazonaws.com:3306/";
+    private static final String DB_USER = "admin";
+    private static final String DB_PASSWORD = "avihan123";
 
     public String updateSalary(int empid, double percentageIncrease) {
         Connection connection;
